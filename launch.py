85a734fb03c2deae86294943927e8167f2362b56
Erchercog <frontavic@mail.ru>
2018-07-02 13:20:54 +0300

    0.0.05

--- launch.py
+++ launch.py
@@ -9,78 +9,87 @@ from keras.models import model_from_json
 import test
 import OutputNeuroNetwork as onn
 
-print("Загружаю сеть из файлов")
-
-# Загружаем данные об архитектуре сети
-
-json_file = open("mnist_model.json", "r")
-loaded_model_json = json_file.read()
-json_file.close()
-
-# Создаем модель
-loaded_model = model_from_json(loaded_model_json)
-
-# Загружаем сохраненные веса в модель
-loaded_model.load_weights("mnist_model.h5")
-
-print("Загрузка сети завершена")
-
-# Загружаем данные
-X = test.main()
-
-# Компилируем загруженную модель
-loaded_model.compile(loss="categorical_crossentropy", optimizer="SGD", metrics=["accuracy"])
-
-# Оцениваем качество обучения сети загруженной сети на тестовых данных
-output = loaded_model.predict(X, batch_size=200, verbose=2, steps=None)
-
-#output = output / 255
-
-answer = onn.OutputNeuroNetwork(output)
-i = 0
-while i == 0:
-        i = 1
-        userAnsSTR = input('Даже если ответ нейронной сети совпадает,' 
-                      '\nвсё-равно введите ответ, '
-                      '\nэто поможет нейросети развиваться: ')
-        try:
-                userAns = int(userAnsSTR)
-        except:
-                i = 0
-
-while userAns > 10 and userAns <= 0:
-        userAns = int(input('Ответ неправильный, введите его заново '
-                            '(результат должен быть от 0 до 9): '))
-#4Преобразуем ответ к нужному виду
-Check = np.uint8([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
-Check[0][userAns] = 1
-
-if userAns != answer:
-        print('Нейросеть обучается...')
-
-        loaded_model.fit(X, Check, epochs=2, batch_size=200)
+def main():
+        print("Загружаю сеть из файлов")
         
-        print("Сохраняем сеть")
-
-        # Сохраняем сеть для последующего использования
-        # Генерируем описание модели в формате json
-        model_json = loaded_model.to_json()
-        json_file = open("mnist_model.json", "w")
+        # Загружаем данные об архитектуре сети
         
-        # Записываем архитектуру сети в файл
-        json_file.write(model_json)
+        json_file = open("mnist_model.json", "r")
+        loaded_model_json = json_file.read()
         json_file.close()
         
-        # Записываем данные о весах в файл
-        loaded_model.save_weights("mnist_model.h5")
+        # Создаем модель
+        loaded_model = model_from_json(loaded_model_json)
         
-        print("Сохранение сети завершено")
+        # Загружаем сохраненные веса в модель
+        loaded_model.load_weights("mnist_model.h5")
         
+        print("Загрузка сети завершена")
+        
+        # Загружаем данные
+        X = test.main()
+        
+        # Компилируем загруженную модель
+        loaded_model.compile(loss="categorical_crossentropy", optimizer="SGD", metrics=["accuracy"])
+        
+        # Оцениваем качество обучения сети загруженной сети на тестовых данных
         output = loaded_model.predict(X, batch_size=200, verbose=2, steps=None)
+        
+        #output = output / 255
         
         answer = onn.OutputNeuroNetwork(output)
         
-print('Нейросеть окончила свою работу')
+        
+        i = 0
+        while i == 0:
+                i = 1
+                userAnsSTR = input('Даже если ответ нейронной сети совпадает,' 
+                              '\nвсё-равно введите ответ, '
+                              '\nэто поможет нейросети развиваться: ')
+                try:
+                        userAns = int(userAnsSTR)
+                except:
+                        i = 0
+        
+        while userAns > 10 and userAns <= 0:
+                userAns = int(input('Ответ неправильный, введите его заново '
+                                    '(результат должен быть от 0 до 9): '))
+        
+        #4Преобразуем ответ к нужному виду
+        Check = np.uint8([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+        Check[0][userAns] = 1
+        
+        if userAns != answer:
+                print('Нейросеть обучается...')
+        
+                loaded_model.fit(X, Check, epochs=2, batch_size=200)
+                
+                print("Сохраняем сеть")
+        
+                # Сохраняем сеть для последующего использования
+                # Генерируем описание модели в формате json
+                model_json = loaded_model.to_json()
+                json_file = open("mnist_model.json", "w")
+                
+                # Записываем архитектуру сети в файл
+                json_file.write(model_json)
+                json_file.close()
+                
+                # Записываем данные о весах в файл
+                loaded_model.save_weights("mnist_model.h5")
+                
+                print("Сохранение сети завершено")
+                
+                output = loaded_model.predict(X, batch_size=200, verbose=2, steps=None)
+                
+                answer = onn.OutputNeuroNetwork(output)
+                
+        print('Нейросеть окончила свою работу')
+        
+        return answer
+
+if __name__ == '__main__':
+    main()
         
         
     
